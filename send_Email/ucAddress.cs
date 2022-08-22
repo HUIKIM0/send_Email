5c8a045bfe9df6241b38016c4a2885f9eb8ca5f4
HUIKIM0 <fehs123@naver.com>
2022-08-22 15:29:14 +0900

    update

--- send_Email/ucAddress.cs
+++ send_Email/ucAddress.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -27,22 +28,58 @@ namespace send_Email
             InitializeComponent();
         }
 
-        private void btnAdd_Click(object sender, EventArgs e)
-        {
-
-        }
-
         /// <summary>
-        /// ListBox에 
+        /// Button 클릭하면 ListBox에 입력한 메일주소 추가됨 
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (EmailFormatCheck())
+            {
+                if(ListAdress != null && ListAdress.Contains(tboxAddress.Text))  
+                {
+                    MessageBox.Show("이미 등록되어 있는 메일주소 입니다.");
+                    return;  //기존에 있으면 빠꾸
+                }
+
+                lboxAddress.Items.Add(tboxAddress.Text);
+                lboxAddress.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("입력 값이 메일주소 형식이 아닙니다.");
+            }
+            lboxAddress.Items.Add(tboxAddress.Text);
+        }
+
+
         private void lboxAddress_MouseDoubleClick(object sender, MouseEventArgs e)
         {
             int iRow = lboxAddress.SelectedIndex;
             if(iRow >=0 )
             {
                 lboxAddress.Items.RemoveAt(iRow);
+            }
+        }
+
+
+        /// <summary>
+        /// Mail 주소 형식이 맞는지 정규식Regex 체크
+        /// </summary>
+        /// <returns></returns>
+        private bool EmailFormatCheck()
+        {
+            string regExp = @"[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?";  // 이메일 정규식 체크
+            Regex regex = new Regex(regExp);
+
+            if (regex.IsMatch(tboxAddress.Text))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
     }
